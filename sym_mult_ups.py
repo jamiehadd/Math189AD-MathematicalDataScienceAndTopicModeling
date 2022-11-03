306707a95d87dccca6d8a4299fbef295d340ef29
Jamie Haddock <jamie.lynn.haddock@gmail.com>
2022-11-03 10:00:50 -0700

    Update sym_mult_ups.py

--- sym_mult_ups.py
+++ sym_mult_ups.py
@@ -13,16 +13,16 @@ def sym_mult_ups(A, k, alpha = 1, M = 10):
   W = np.abs(np.random.randn(n,k))                                  #initialize factor matrices
   H = np.abs(np.random.randn(k,n))
   
-  errors = [np.linalg.norm(A-np.transpose(H)@H,'fro')**2]           #initialize error array
+  #errors = [np.linalg.norm(A-np.transpose(H)@H,'fro')**2]           #initialize error array
   
-  for i in range(M):
-    Abar = np.vstack((A,np.sqrt(alpha)*np.transpose(W)))
-    Wbar = np.vstack((W,np.sqrt(alpha)*np.eye(k)))
+  #for i in range(M):
+  #  Abar = np.vstack((A,np.sqrt(alpha)*np.transpose(W)))
+  #  Wbar = np.vstack((W,np.sqrt(alpha)*np.eye(k)))
 
-    H = H*((np.transpose(Wbar)@Abar)/(np.transpose(Wbar)@Wbar@H))
+  #  H = H*((np.transpose(Wbar)@Abar)/(np.transpose(Wbar)@Wbar@H))
                                                                     #update for H    
-    W = np.transpose(H)                                             #update W     
+  #  W = np.transpose(H)                                             #update W     
     
-    errors.append(np.linalg.norm(A-np.transpose(H)@H,'fro')**2)     #record error
+  #  errors.append(np.linalg.norm(A-np.transpose(H)@H,'fro')**2)     #record error
   
-  return np.transpose(H), errors
+  return np.transpose(H)#, errors
