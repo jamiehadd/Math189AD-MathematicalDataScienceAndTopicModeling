13a403a97945c95dabc884f98ed2eb7b19e1b86e
Jamie Haddock <jamie.lynn.haddock@gmail.com>
2022-11-03 09:58:49 -0700

    Update sym_mult_ups.py

--- sym_mult_ups.py
+++ sym_mult_ups.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def sym_mult_ups(A, k, alpha = 1, M = 10):
+  print("new version")
   eps_divide = 1e-6                                                 #define a small number to ensure we never divide by 0
   n,n = np.shape(A)                                                 #determine the size of X for initializing A and S
   
@@ -23,6 +24,5 @@ def sym_mult_ups(A, k, alpha = 1, M = 10):
     W = np.transpose(H)                                             #update W     
     
     errors.append(np.linalg.norm(A-np.transpose(H)@H,'fro')**2)     #record error
-    print("new version")
   
   return np.transpose(H), errors
