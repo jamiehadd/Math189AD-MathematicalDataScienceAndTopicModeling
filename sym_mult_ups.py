c85aa71b74acb5b5413924e2267fee84109447af
Jamie Haddock <jamie.lynn.haddock@gmail.com>
2022-11-03 10:44:48 -0700

    Update sym_mult_ups.py

--- sym_mult_ups.py
+++ sym_mult_ups.py
@@ -13,7 +13,7 @@ def sym_mult_ups(A, k, alpha = 1, M = 10):
   W = np.abs(np.random.randn(n,k))                                  #initialize factor matrices
   H = np.abs(np.random.randn(k,n))
   
-  errors = [np.linalg.norm(A-np.transpose(H)@H,'fro')**2]           #initialize error array
+  #errors = [np.linalg.norm(A-np.transpose(H)@H,'fro')**2]           #initialize error array
   
   for i in range(M):
     Abar = np.vstack((A,np.sqrt(alpha)*np.transpose(W)))
@@ -23,6 +23,6 @@ def sym_mult_ups(A, k, alpha = 1, M = 10):
                                                                     #update for H    
     W = np.transpose(H)                                             #update W     
     
-    errors.append(np.linalg.norm(A-np.transpose(H)@H,'fro')**2)     #record error
+    #errors.append(np.linalg.norm(A-np.transpose(H)@H,'fro')**2)     #record error
   
-  return np.transpose(H), errors
+  return np.transpose(H)#, errors
